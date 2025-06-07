00e2cd131c755f462bde1394dd0277f000e084ac
Jakub Flak <flak.jakub@gmail.com>
2025-06-07 18:25:32 +0200

    Added feature of word cloud plot

--- manager.py
+++ manager.py
@@ -6,6 +6,10 @@ import re
 from collections import Counter
 from lyricsgenius import Genius
 import pandas as pd
+import numpy as np
+from PIL import Image
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 
 def main():
     
@@ -51,7 +55,7 @@ def main():
         words = re.findall(r"\b\w+\b", lyrics.lower())
 
 
-        number_of_desired_words = 10
+        number_of_desired_words = 20
 
         meaningful_words = [word for word in words if word not in stopwords.english]  
         meaningful_words_counter = Counter(meaningful_words)
@@ -59,6 +63,33 @@ def main():
         
         word_frequency_df = pd.DataFrame(most_frequent_meaningful_words, columns = ['word', 'word_count'])
         word_frequency_df.sort_values(by='word_count').plot(x='word',kind='barh', title=f"{artist} - {title}\n Most Frequent Words")
+        print('Plot created!')
+    
+    
+    def song_words_cloud():
+        artist = "Damiano David"
+        title = "Next Summer"
+        
+        genius.remove_section_headers = True
+        song = genius.search_song(title, artist)
+        
+        lyrics = '\n'.join(song.lyrics.split('\n')[1:])
+        
+        words = re.findall(r"\b\w+\b", lyrics.lower())
+
+
+        number_of_desired_words = 20
+
+        meaningful_words = [word for word in words if word not in stopwords.english]  
+        meaningful_words_counter = Counter(meaningful_words)
+        most_frequent_meaningful_words = meaningful_words_counter.most_common(number_of_desired_words)
+        
+        
+        wc = WordCloud(background_color='black', colormap='viridis', 
+                       width = 800, height = 500).generate_from_frequencies(meaningful_words_counter)
+        plt.axis("off")
+        plt.imshow(wc)
+
         print('Plot created!')
     
     
@@ -73,6 +104,7 @@ def main():
         "1": show_song_lyrics,
         "2": save_song_lyrics,
         "3": song_words_histogram,
+        "4": song_words_cloud,
         "6": exit_program
     }
     
@@ -83,6 +115,7 @@ def main():
         print(" 1. Show song lyrics")
         print(" 2. Save song lyrics to a file")
         print(" 3. Show song words histogram")
+        print(" 4. Show song words cloud")
         print("6. Exit\n")
     
         choice = input("Choose an option: ").strip()
